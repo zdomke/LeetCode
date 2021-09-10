99c2a590939058e0f5baf44b5feb1e665807750a
Zattach <zach@rozzle.com>
2021-09-10 15:13:15 -0700

    Update path-sum.py

--- Easy/path-sum.py
+++ Easy/path-sum.py
@@ -4,17 +4,19 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
-class Solution:
+class Solution:     # 51ms
     def recursion(self, node: Optional[TreeNode], targetSum: int) -> bool:
         if node.left == None and node.right == None:
             return node.val == targetSum
         left = False
         if(node.left != None):
             left = self.recursion(node.left, targetSum - node.val)
+            if left:
+                return True
         right = False
         if(node.right != None):
             right = self.recursion(node.right, targetSum - node.val)
-        return left or right
+        return right
     
     def hasPathSum(self, root: Optional[TreeNode], targetSum: int) -> bool:
         if root == None:
