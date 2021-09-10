62ca1c3778cdf8ba2105a0dac2ef65d16f27d9f1
Zattach <zach@rozzle.com>
2021-09-10 13:03:44 -0700

    Update count-good-triplets.py

--- Easy/count-good-triplets.py
+++ Easy/count-good-triplets.py
@@ -1,9 +1,16 @@
-class Solution:
+class Solution:     # 533ms
     def countGoodTriplets(self, arr: List[int], a: int, b: int, c: int) -> int:
+        dynArr = []
+        for i in range(len(arr) - 1):
+            temp = [0] * len(arr)
+            for j in range(i, len(arr)):
+                temp[j] = abs(arr[i] - arr[j])
+            dynArr.append(temp)
+                
         ret = 0
         for i in range(len(arr) - 2):
             for j in range(i + 1, len(arr) - 1):
                 for k in range(j + 1, len(arr)):
-                    if abs(arr[i] - arr[j]) <= a and abs(arr[j] - arr[k]) <= b and abs(arr[i] - arr[k]) <= c:
+                    if dynArr[i][j] <= a and dynArr[j][k] <= b and dynArr[i][k] <= c:
                             ret += 1
         return ret
