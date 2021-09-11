45a13ca4e89760bbc308236b8f6f75a90bc9639b
Zattach <zach@rozzle.com>
2021-09-10 19:22:02 -0700

    Update self-dividing-numbers.java

--- Easy/self-dividing-numbers.java
+++ Easy/self-dividing-numbers.java
@@ -1,16 +1,27 @@
-class Solution{		// 14ms
+class Solution{		// 5ms
     public List<Integer> selfDividingNumbers(int left, int right){
+    	boolean[] digits = new boolean[10];
     	List<Integer> ret = new ArrayList<>();
 
     	for(int i = left; i <= right; i++){
+            for(int j = 0; j < 10; j++)
+                digits[j] = false;
+	    	int j = i;
+	    	while(j > 0){
+	    		digits[j % 10] = true;
+	    		j /= 10;
+	    	}
     		boolean bool = true;
-    		char[] arr = String.valueOf(i).toCharArray();
-    		for(int j = 0; j < arr.length; j++){
-    			if((arr[j] == '0') || (i % Character.getNumericValue(arr[j]) != 0)){
-    				bool = false;
-    				break;
-    			}
-    		}
+	    	if(!digits[0]){
+	    		for(j = 1; j < 10; j++){
+	    			if(digits[j] && i % j != 0){
+	    				bool = false;
+	    				break;
+	    			}
+	    		}
+	    	} else {
+	    		bool = false;
+	    	}
     		if(bool)
 	    		ret.add(i);
     	}
