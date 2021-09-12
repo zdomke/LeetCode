73fd0a95b86ff3f59340b4eb92db9377c22ddb9c
Zattach <zach@rozzle.com>
2021-09-12 15:34:07 -0700

    Create divisor-game

---
+++ Easy/divisor-game.java
@@ -0,0 +1,6 @@
+class Solution {        // 0ms
+    public boolean divisorGame(int n) {
+        if(n % 2 == 0) return true;
+        return false;
+    }
+}
---
+++ Easy/divisor-game.py
@@ -0,0 +1,5 @@
+class Solution:     # 32ms
+    def divisorGame(self, n: int) -> bool:
+        if n % 2 == 0:
+            return True
+        return False
