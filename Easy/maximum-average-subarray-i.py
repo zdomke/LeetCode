725181f1144ef82747d6739b98348c3c9bf750a8
Zattach <zach@rozzle.com>
2021-09-12 16:13:25 -0700

    Update maximum-average-subarray-i.py

--- Easy/maximum-average-subarray-i.py
+++ Easy/maximum-average-subarray-i.py
@@ -1,10 +1,10 @@
-class Solution:     # 1409ms
+class Solution:     # 1208ms
     def findMaxAverage(self, nums: List[int], k: int) -> float:
         prev = 0
         for i in range(k):
             prev += nums[i]
-        ret = prev / k
+        ret = prev
         for i in range(len(nums) - k):
             prev = prev - nums[i] + nums[i + k]
-            ret = max(prev / k, ret)
-        return ret
+            ret = max(prev, ret)
+        return ret / k
