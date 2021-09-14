40dd18a56080f68189851690a1e65f02e9d33590
Zattach <zach@rozzle.com>
2021-09-14 15:13:38 -0700

    Update goat-latin.java

--- Easy/goat-latin.java
+++ Easy/goat-latin.java
@@ -1,15 +1,15 @@
-class Solution {        // 17ms
+class Solution {        // 7ms
     public String toGoatLatin(String sentence) {
-        String ret = "";
         Set<Character> vowel = new HashSet<>(Arrays.asList('a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'));
-        
+        StringBuilder ret = new StringBuilder();
+
         int j = 0, i = 0;
         for(String w : sentence.split(" ")){
-            ret += ' ' + (vowel.contains(w.charAt(0)) ? w : w.substring(1) + w.charAt(0)) + "ma";
+            ret.append(' ' + (vowel.contains(w.charAt(0)) ? w : w.substring(1) + w.charAt(0)) + "ma");
             for(j = 0, i++; j < i; j++){
-                ret += 'a';
+                ret.append('a');
             }
         }
-        return ret.substring(1);
+        return ret.toString().substring(1);
     }
 }
