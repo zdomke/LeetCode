eb783aa00ef2e25cc0286219ed966caa492e7875
Zattach <zach@rozzle.com>
2021-09-14 14:33:31 -0700

    Update goat-latin.py

--- Easy/goat-latin.py
+++ Easy/goat-latin.py
@@ -1,14 +1,15 @@
-class Solution:     # 45ms
+class Solution:     # 32ms
     def toGoatLatin(self, sentence: str) -> str:
         sentence = sentence.split()
         ret = ""
         for i in range(len(sentence)):
             first = sentence[i][0]
             if first.lower() not in 'aeiou':
-                sentence[i] = sentence[i][1:] + sentence[i][0]
+                sentence[i] = sentence[i][1:] + first
             sentence[i] += 'maa'
+            ret += sentence[i]
             for _ in range(i):
-                sentence[i] += 'a'
-            ret += sentence[i] + ' '
+                ret += 'a'
+            ret += ' '
                 
         return ret[:-1]
