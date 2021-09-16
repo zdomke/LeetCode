c4d52adb1f98a8b1e2cee67038cbc0b18a8a958b
Zattach <zach@rozzle.com>
2021-09-16 11:54:21 -0700

    Update valid-mountain-array.java

--- Easy/valid-mountain-array.java
+++ Easy/valid-mountain-array.java
@@ -1,11 +1,19 @@
 class Solution {        // 1ms
     public boolean validMountainArray(int[] arr) {
         int n = arr.length;
-        int i = 0;
+        if(n < 3) return false;
+        int first = 0;
+        int last = n - 1;
         
-        for(; i + 1 < n && arr[i] < arr[i + 1]; i++);
-        if(i == 0 || i + 1 == n) return false;
-        for(; i + 1 < n && arr[i] > arr[i + 1]; i++);
-        return i == n - 1;
+        while(first < last){
+            if(arr[first] < arr[first + 1]){
+                first++;
+            } else if(arr[last] < arr[last - 1]){
+                last--;
+            } else {
+                break;
+            }
+        }
+        return (first != 0) && (last != n - 1) && (first == last);
     }
 }
