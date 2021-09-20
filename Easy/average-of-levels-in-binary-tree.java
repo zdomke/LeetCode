48bfad16d16e9c3496f1e35f85c7c68d41dd2fbb
Zattach <zach@rozzle.com>
2021-09-20 12:33:23 -0700

    Update average-of-levels-in-binary-tree.java

--- Easy/average-of-levels-in-binary-tree.java
+++ Easy/average-of-levels-in-binary-tree.java
@@ -13,26 +13,24 @@
  *     }
  * }
  */
-class Solution {        // 4ms
+class Solution {        // 3ms
     public List<Double> averageOfLevels(TreeNode root) {
         List<Double> ret = new ArrayList<>();
         Queue<TreeNode> q = new LinkedList<>();
         q.add(root);
         
         while(!q.isEmpty()){
-            long sum = 0, count = 0;
-            Queue<TreeNode> temp = new LinkedList<>();
-            while(!q.isEmpty()){
-                TreeNode node = q.remove();
+            double sum = 0;
+            int size = q.size();
+            for(int i = 0; i < size; i++){
+                TreeNode node = q.poll();
                 sum += node.val;
-                count++;
                 if(node.left != null)
-                    temp.add(node.left);
+                    q.add(node.left);
                 if(node.right != null)
-                    temp.add(node.right);
+                    q.add(node.right);
             }
-            q = temp;
-            ret.add(1.0 * sum / count);
+            ret.add(sum / size);
         }
         return ret;
     }
