7defcb1f7ace6e2dfc37f64b2c51563a94ea701a
Zattach <zach@rozzle.com>
2024-07-22 22:09:38 -0700

    Solution for solving-questions-with-brainpower

--- Medium/solving-questions-with-brainpower.py
+++ Medium/solving-questions-with-brainpower.py
@@ -4,38 +4,21 @@ class Solution(object):
         :type questions: List[List[int]]
         :rtype: int
         """
-        # ans = set()
-        # skip = set()
-
-        # for i in range(len(questions)):
-        #     if i in skip:
-        #         continue
-        #     j = i
-        #     run = 0
-        #     while j < len(questions):
-        #         q = questions[j]
-        #         run += q[0]
-        #         j += q[1] + 1
-        #         skip.add(j)
-        #     ans.add(run)
-        # return max(ans)
-
-        if len(questions) == 1:
-            return questions[0]
-
-        my_q = []
-        for q in questions[1:]:
-            pass
-        return max([self.mostPoints(questions[i:]) for i in range(0, len(questions))])
+        ans = [0] * (len(questions) + 1)
+        for i in range(len(questions) - 1, -1, -1):
+            pt, bp = questions[i]
+            next_q = i + bp + 1
+            next_q = min(next_q, len(questions))
+            ans[i] = max(ans[i+1], pt + ans[next_q])
+        return ans[0]
 
 
 if __name__ == "__main__":
     sol = Solution()
     tests = [[[3, 2], [4, 3], [4, 4], [2, 5]],
-             [[1, 1], [2, 2], [3, 3], [4, 4], [5, 5]]]
+             [[1, 1], [2, 2], [3, 3], [4, 4], [5, 5]],
+             [[12, 46], [78, 19], [63, 15], [79, 62], [13, 10]],
+             [[21, 5], [92, 3], [74, 2], [39, 4], [58, 2], [5, 5], [49, 4], [65, 3]]]
     for te in tests:
         res = sol.mostPoints(te)
         print(f"{te}: --> {res}")
-
-
-# https://leetcode.com/problems/solving-questions-with-brainpower/submissions/1330117296/
