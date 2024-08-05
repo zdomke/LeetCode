ef2371ca734f03a77049d435ef8a2fbd1c813288
Zattach <zach@rozzle.com>
2024-08-04 19:25:22 -0700

    Better solution for kth-distint-string-in-an-array

--- Easy/kth-distinct-string-in-an-array.py
+++ Easy/kth-distinct-string-in-an-array.py
@@ -5,9 +5,16 @@ class Solution:
         :type k: int
         :rtype: str
         """
+        count = {}
+        for s in arr:
+            if s not in count:
+                count[s] = 1
+            else:
+                count[s] += 1
+
         dist = 0
         for s in arr:
-            if arr.count(s) == 1:
+            if count[s] == 1:
                 dist += 1
                 if dist == k:
                     return s
