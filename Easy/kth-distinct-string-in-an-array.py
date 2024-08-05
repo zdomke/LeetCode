af2603983a3a717e4c8645d2806300bcd0f00222
Zattach <zach@rozzle.com>
2024-08-04 19:20:39 -0700

    Clean up kth-distinct-string-in-an-array

--- Easy/kth-distinct-string-in-an-array.py
+++ Easy/kth-distinct-string-in-an-array.py
@@ -1,7 +1,26 @@
 class Solution:
     def kthDistinct(self, arr: list[str], k: int) -> str:
+        """
+        :type arr: List[str]
+        :type k: int
+        :rtype: str
+        """
         dist = 0
         for s in arr:
-            dist = dist + 1 if arr.count(s) == 1 else dist
-            if dist == k: return s
+            if arr.count(s) == 1:
+                dist += 1
+                if dist == k:
+                    return s
         return ""
+
+
+if __name__ == "__main__":
+    sol = Solution()
+    tests = [
+        (["d", "b", "c", "b", "c", "a"], 2),
+        (["aaa", "aa", "a"], 1),
+        (["a", "b", "a"], 3),
+    ]
+    for te in tests:
+        res = sol.kthDistinct(*te)
+        print(f"{te}: --> {res}")
